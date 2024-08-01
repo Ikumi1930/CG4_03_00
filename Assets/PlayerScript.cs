964136d0f5343b8f286e24ef31b949e62aeac09f
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2024-08-01 10:17:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = 2.0f;
+        float stageMax = 4.0f;
+        //右キー入力で移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if(transform.position.x < stageMax)
+            {
+                rb.velocity = new Vector3(moveSpeed, 0, 0);
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+
+        //左キー入力で移動
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x < stageMax)
+            {
+                rb.velocity = new Vector3(-moveSpeed, 0, 0);
+            } 
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+}
